6e327fb63769d98c3ac22958b94db4d7949194d2
Marcos Pereira <marcosppereira16@hotmail.com>
2018-09-04 15:51:46 -0300

    Alterações

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,14 @@
-console.log("Fui carregado de um arquivo externo !");
-
 var titulo = document.querySelector("h1");
-
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = peso / altura * altura; // imc = 100 / 2.0 * 2.0 = 100 / 4 => 25
+
+console.log(imc);
